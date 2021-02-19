e9760cb672593b138614d0bfd073ce47311dc51d
何常宇 <1104397151@qq.com>
2021-02-19 14:24:04 +0800

    maintabbar has been completed

---
+++ src/views/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    home
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Home",
+        components:{
+          
+        },
+    }
+</script>
+
+<style scoped>
+
+
+
+</style>
